6a34ee33c2183c65acd095441b4cf5e8d9d8c696
nhatnvph15151 <nnhat1802@gmail.com>
2022-02-21 12:38:22 +0700

    fisrt commit

--- src/components/header.js
+++ src/components/header.js
@@ -6,17 +6,17 @@ const header = {
         <nav class=" m-auto max-w-7xl flex justify-between items-center bg-white  ">
           <img src="https://file.hstatic.net/200000397757/file/logo_8319db4a3fb145d39c8cb6c697d41bca.png" alt="">
           <ul class="flex text-lg ">
-            <li class="pr-3 pl-3 hover:text-orange-500"><a href="/">Trang Chủ</a></li>
-            <li class="pr-3 pl-3 hover:text-orange-500"><a href="/lists">Sản Phẩm</a></li>
-            <li class="pr-3 pl-3 hover:text-orange-500"><a href="/details">Tin Tức</a></li>
-            <li class="pr-3 pl-3 hover:text-orange-500"><a href="/admin/doashboard">Liên Hệ</a></li>
+            <li class="pr-3 pl-3 hover:text-orange-500"><a href="#/">Trang Chủ</a></li>
+            <li class="pr-3 pl-3 hover:text-orange-500"><a href="#/lists">Sản Phẩm</a></li>
+            <li class="pr-3 pl-3 hover:text-orange-500"><a href="#/details">Tin Tức</a></li>
+            <li class="pr-3 pl-3 hover:text-orange-500"><a href="#/admin/doashboard">Liên Hệ</a></li>
           </ul>
           <ul class="flex text-lg ">
             
            
-            <li class="pr-3 pl-3 mr-2 border rounded-md text-[14px] hover:bg-orange-400 hover:text-white "><a href="/signup">Đăng Ký</a></li>
-            <li class="pr-3 pl-3 border rounded-md text-[14px] hover:bg-orange-400 hover:text-white " ><a href="/signin">Đăng Nhập</a></li>
-            <li class="pr-3 pl-3"><a href="/cart"><i class="fa-solid fa-cart-shopping"></i></a></li>
+            <li class="pr-3 pl-3 mr-2 border rounded-md text-[14px] hover:bg-orange-400 hover:text-white "><a href="#/signup">Đăng Ký</a></li>
+            <li class="pr-3 pl-3 border rounded-md text-[14px] hover:bg-orange-400 hover:text-white " ><a href="#/signin">Đăng Nhập</a></li>
+            <li class="pr-3 pl-3"><a href="#/cart"><i class="fa-solid fa-cart-shopping"></i></a></li>
             <li class="pr-3 pl-3"><a href="#"><i class="fa-solid fa-magnifying-glass"></i></a></li>
             
             </ul>
--- src/components/headerAdmin.js
+++ src/components/headerAdmin.js
@@ -11,8 +11,8 @@ const headerAdmin ={
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="/admin/doashboard" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Dashboard</a>
-              <a href="/admin/listsAdmin" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
+              <a href="#/admin/doashboard" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Dashboard</a>
+              <a href="#/admin/listsAdmin" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
             </div>
           </div>
         </div>
